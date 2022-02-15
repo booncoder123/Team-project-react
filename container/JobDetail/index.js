c1d500c0fc5cf69694934985c6e75d73d884010c
pploythehuman <kanokpon26@gmail.com>
2022-02-15 11:44:24 +0700

    add files

---
+++ container/JobDetail/index.js
@@ -0,0 +1,9 @@
+import classes from "./index.module.css";
+
+export default function JobDetailContainer() {
+    return(
+        <div className={classes.block}>
+            <p>test</p>
+        </div>
+    );
+}
--- pages/Feed/Jobs/JobDetails/index.js
+++ pages/Feed/Jobs/JobDetails/index.js
@@ -1,13 +1,13 @@
 import classes from "./index.module.css";
 import FeatureDropDown from "/container/Feed/FeatureDropDown";
+import JobDetailContainer from "../../../../container/JobDetail"
 
-
-export default function SignIn() {
+export default function JobDetail() {
     return(
       <div className={classes.bg}>
-        { <div className={classes.header}>
-            <FeatureDropDown />
-        </div> }
+        <div>
+          <JobDetailContainer/>
+        </div>
         
       </div>
     );
