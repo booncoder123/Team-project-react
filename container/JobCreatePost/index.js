aee4f47e4415f291928a5b737abfa1418532d8f9
pploythehuman <kanokpon26@gmail.com>
2022-02-24 16:24:29 +0700

    create feature post job

---
+++ container/JobCreatePost/index.js
@@ -0,0 +1,13 @@
+import Test from "../../components/Draft"
+import TextField from "../../components/TextField"
+
+
+export default function JobCreatePost(props) {
+    const {company,position,detail} = props;
+      return (
+        <div>
+            <TextField/>
+        </div>
+      )
+    }
+    
---
+++ pages/Feed/Jobs/JobCreatePost/index.js
@@ -0,0 +1,14 @@
+// import classes from "./index.module.css";
+// // import JobCreatePost from "../../../container/JobCreatePost";
+// import JobCreatePost from "../../../../components/Draft"
+// import { useState } from "react";
+
+// export default function JobCreatePost() {
+//     const [postMessage, setPostMessage] = useState("");
+//     const [type, setType] = useState(0)
+//         return(
+//           <div className={classes.bg}>
+//             <JobCreatePost/>
+//           </div>
+//         );
+//     }
