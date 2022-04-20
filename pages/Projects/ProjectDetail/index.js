2579113eb048ba058f33fc385210c2119b1e8ce0
nnevermine <62011192@kmitl.ac.th>
2022-04-20 18:11:59 +0700

    feat[connectAPItoProjectDetail]

--- pages/Projects/ProjectDetail/index.js
+++ pages/Projects/ProjectDetail/index.js
@@ -3,35 +3,67 @@ import { useRouter } from "next/router";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { discussions } from "../../../const/mockUp.js";
 import withAuth from "../../../helpers/withAuth";
+import Projects from "../../../lib/api/projects";
+import { parseCookies } from "../../../helpers/cookie";
 
-
-
-function ProjectDetail() {
+function ProjectDetail(props) {
   const router = useRouter();
   function nextPagehandler() {
     router.push("/Projects");
   }
-  return(
+  console.log("propsss", props);
+  return (
     <div className={classes.container}>
-      <div className={classes.title}>Quiz Space</div> 
+      <div className={classes.title}>
+        {props.project.data[0].project[0].name}
+      </div>
       <div className={classes.banner}>
         <ArrowBackIosIcon sx={{ fontSize: 15 }} onClick={nextPagehandler} />
         Projects
-      </div> 
+      </div>
+      {props.project.data[0].images[0] && (
+        <img
+          src={
+            "https://se-community-2022.s3.ap-southeast-1.amazonaws.com/" +
+            props.project.data[0].images[0]
+          }
+          className={classes.images}
+        />
+      )}
       <div>
         {/* <div className={classes.header}>Header</div> */}
         <div className={classes.introduction}>
-          Quiz Space is a web-based examination system where quizzes are taken online i.e. through the internet or using a computer system.
+          {props.project.data[0].project[0].intro}
         </div>
         <div className={classes.description}>
-          The purpose of Quiz Space is to take Quizzes in an efficient manner and not wasting time checking the paper. The main objective of Quiz Space is to efficiently evaluate the candidate through a fully automated system that not only saves a lot of time but also gives fast results. Additionally, a note canvas is prepared in all quizzes in case the candidate needs it for calculation. The score and the answer to the questions will be shown after the candidate finishes the quiz.
-          We apply the web programming knowledge (HTML,CSS) to this project and use React which is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications. It’s used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components for frontend. Also, we use Django which is a Python-based free and open-source web framework that follows the model–template–views (MTV) architectural pattern for backend part.
-          The purpose of Quiz Space is to take Quizzes in an efficient manner and not wasting time checking the paper. The main objective of Quiz Space is to efficiently evaluate the candidate through a fully automated system that not only saves a lot of time but also gives fast results. Additionally, a note canvas is prepared in all quizzes in case the candidate needs it for calculation. The score and the answer to the questions will be shown after the candidate finishes the quiz.
-          We apply the web programming knowledge (HTML,CSS) to this project and use React which is an open-source JavaScript library that is used for building user interfaces specifically for single-page applications. It’s used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components for frontend. Also, we use Django which is a Python-based free and open-source web framework that follows the model–template–views (MTV) architectural pattern for backend part.
+          {props.project.data[0].project[0].projectDescription}
         </div>
       </div>
     </div>
-      
   );
 }
 export default withAuth(ProjectDetail);
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const cookies = parseCookies(req);
+  const { token } = cookies;
+  const projectId = context.query.projectId;
+  console.log("projectID", projectId);
+
+  try {
+    const project = await Projects.get({
+      type: Projects.GET_PROJECT_DETAIL,
+      token,
+      body: {
+        projectId,
+      },
+    });
+    return {
+      props: { token, project: project.data },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+  return { props: {} };
+}
