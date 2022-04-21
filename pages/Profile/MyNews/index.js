6a85b6b3fda48432a816aed9dfef3ad888d0ee8b
nnevermine <62011192@kmitl.ac.th>
2022-04-21 16:41:31 +0700

    feature[myNews-profilePage]

---
+++ pages/Profile/MyNews/index.js
@@ -0,0 +1,37 @@
+import classes from "./index.module.css";
+import { discussions } from "../../../const/mockUp.js";
+import Discussion from "../../../container/Discussion";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { useRouter } from "next/router";
+import withAuth from "../../../helpers/withAuth";
+
+function MyNews() {
+  const router = useRouter();
+  function nextPagehandler() {
+    router.push("/Profile");
+  }
+  return (
+    <div className={classes.container}>
+      <div className={classes.title}>My News</div>
+      <div className={classes.banner}>
+        <ArrowBackIosIcon sx={{ fontSize: 15 }} onClick={nextPagehandler} />
+        My Posts
+      </div>
+      <div className={classes.content}>
+          news stuff
+        {/* {discussions.map((discussion) => {
+          return (
+            <Discussion
+              title={discussion.title}
+              images={discussion.images}
+              like={discussion.like}
+              comment={discussion.comment}
+            />
+          );
+        })} */}
+      </div>
+    </div>
+  );
+}
+
+export default withAuth(MyNews);
