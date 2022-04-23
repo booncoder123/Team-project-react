b7cd8758f99944a1db50d12bb3c79f7e9c636ce2
Kanokpon <kanokpon26@gmail.com>
2022-04-24 01:25:05 +0700

    import for news and discussions

--- pages/Feed/Discussions/CreateDiscussion/index.js
+++ pages/Feed/Discussions/CreateDiscussion/index.js
@@ -1,5 +1,24 @@
-export default function CreateDiscussion() {
-    return(
-        <div>This is create discussion page</div>
+import classes from "./index.module.css";
+import TextField from "../../../../components/TextField";
+import Dropdown from "../../../../components/Dropdown";
+import RectangularButton from "../../../../components/RectangularButton";
+import SingleImageUpload from "../../../../components/SingleImageUpload";
+import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import withAuth from "../../../../helpers/withAuth";
+import { parseCookies } from "../../../../helpers/cookie";
+import Discussion from "../../../../lib/api/discussions";
+import WYSIWYGEditor from "../../../../components/Editor";
+
+
+const CreateDiscussion = (props) => {
+    
+    return (
+      <div>
+          This is create discussion page
+      </div>
     );
-}
+  };
+  
+  export default withAuth(CreateDiscussion);
+  
--- pages/Feed/News/CreateNews/index.js
+++ pages/Feed/News/CreateNews/index.js
@@ -1,5 +1,24 @@
-export default function CreateNews() {
-    return(
-        <div>This is create news page</div>
+import classes from "./index.module.css";
+import TextField from "../../../../components/TextField";
+import Dropdown from "../../../../components/Dropdown";
+import RectangularButton from "../../../../components/RectangularButton";
+import SingleImageUpload from "../../../../components/SingleImageUpload";
+import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import withAuth from "../../../../helpers/withAuth";
+import { parseCookies } from "../../../../helpers/cookie";
+import Discussion from "../../../../lib/api/discussions";
+import WYSIWYGEditor from "../../../../components/Editor";
+
+
+const CreateNews = (props) => {
+    
+    return (
+      <div>
+          This is create news page
+      </div>
     );
-}
+  };
+  
+  export default withAuth(CreateNews);
+  
