638b607245499e7f01cf853795cc24f2814d6bc6
nnevermine <62011192@kmitl.ac.th>
2022-05-24 15:11:56 +0700

    remove time

--- components/ProfileTag/index.js
+++ components/ProfileTag/index.js
@@ -5,7 +5,7 @@ export default function ProfileTag(props) {
     <div className={classes.tag}>
       <Avatar alt="Remy Sharp" src= {"https://se-community-2022.s3.ap-southeast-1.amazonaws.com/" +
             props.photo} />
-      <div className={classes.heading1}>{props.name} · 1h </div>
+      <div className={classes.heading1}>{props.name} </div>
     </div>
   );
 }
